32acb4b23e8d949b9042ed233e94f51497934dc1
varrtix <tiamo_nana@outlook.com>
2020-04-16 19:25:37 +0800

    Refactor completed: split view part out of view controller

--- BookCollection/Controllers/BCInfoViewController.swift
+++ BookCollection/Controllers/BCInfoViewController.swift
@@ -27,20 +27,9 @@
 /// THE SOFTWARE.
 
 import UIKit
-import SnapKit
 import Kingfisher
-//
-//protocol BCInfoViewControllerDelegate: class {
-//  func infoViewController(
-//    _ controller: BCInfoViewController, didAdding book: BCBook)
-//
-//  func infoViewController(
-//    _ controller: BCInfoViewController, didRemoving book: BCBook)
-//}
 
 class BCInfoViewController: BCViewController {
-  
-  fileprivate var backgroundHeight: CGFloat = 270.5
   
   fileprivate var topDistance: CGFloat {
     if #available(iOS 13.0, *) {
@@ -54,21 +43,22 @@ class BCInfoViewController: BCViewController {
     }
   }
   
-  fileprivate var backgroundImageView = UIImageView(image: UIImage(named: "Info/NavigationBar"))
+  private lazy var infoView = BCInfoView(frame: view.frame, withScrollView: topDistance)
   
-  var book: BCBook!
+  private let book: BCBook!
   
-  lazy var isMarked = false
+  private let bookShelf = BCBookshelf.shared
+  
+  private let defaultMarked: Bool
   
   init(with book: BCBook) {
-    super.init(nibName: nil, bundle: nil)
-    
     self.book = book
+    self.defaultMarked = book.isMarked
+    
+    super.init(nibName: nil, bundle: nil)
   }
   
   required init?(coder: NSCoder) {
-    super.init(coder: coder)
-    
     fatalError("init(coder:) has not been implemented")
   }
 }
@@ -81,17 +71,27 @@ extension BCInfoViewController {
     
     view.backgroundColor = BCColor.BarTint.white
     
-    configureNavigation()
+    configure(item: navigationItem)
     configureSubviews()
+    
+  }
+  
+  override func viewDidDisappear(_ animated: Bool) {
+    super.viewDidDisappear(animated)
+    
+    if !book.isMarked && defaultMarked {
+      bookShelf.remove(book)
+    } else if book.isMarked && !defaultMarked {
+      bookShelf.append(book)
+    }
   }
 }
 
 // MARK: - View configure
 extension BCInfoViewController {
   
-  fileprivate func configureNavigation() {
-//    navigationItem.title = ?? "Book Information"
-    navigationItem.title = book?.title ?? "Book Information"
+  fileprivate func configure(item: UINavigationItem) {
+    item.title = book?.title ?? "Book Information"
     
     navigationBarBackgroundImage = UIImage()
     
@@ -100,7 +100,7 @@ extension BCInfoViewController {
     shouldHideBottomBarWhenPushed = true
     
     if #available(iOS 13.0, *) {} else {
-      navigationItem.leftBarButtonItem = UIBarButtonItem(
+      item.leftBarButtonItem = UIBarButtonItem(
         title: "Back",
         style: .plain,
         target: self,
@@ -109,84 +109,41 @@ extension BCInfoViewController {
   }
   // MARK: configure Subviews
   fileprivate func configureSubviews() {
-    configureBackgroundView()
-    
-    configureScrollView()
+    infoView.delegate = self
+    view.addSubview(infoView)
+  }
+}
+
+// MARK: - Actions
+@objc
+extension BCInfoViewController {
+  private func close(_ sender: UIBarButtonItem? = nil) {
+    dismiss(animated: true)
+  }
+}
+
+// MARK: - BCInfoView Delegate
+extension BCInfoViewController: BCInfoViewDelegate {
+  var titleText: String {
+    book.title ?? ""
   }
   
-  fileprivate func configureBackgroundView() {
-    view.addSubview(backgroundImageView)
-  }
-  
-  // MARK: Scroll view
-  fileprivate func configureScrollView() {
-    
-    let scrollView = UIScrollView(frame: CGRect(
-      x: 0,
-      y: topDistance,
-      width: self.view.frame.width,
-      height: self.view.frame.height - topDistance
-    ))
-    
-    scrollView.alwaysBounceVertical = true
-    scrollView.delegate = self
-    scrollView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-    
-    view.addSubview(scrollView)
-    
-    // MARK: - The head of scroll view
-    let headView = UIView()
-    
-    scrollView.addSubview(headView)
-    
-    headView.snp.makeConstraints { make in
-      make.top.width.equalToSuperview()
-      make.height.greaterThanOrEqualTo(206.5).priority(750)
-    }
-    // Cover
-    let coverImageView = UIImageView()
-    coverImageView.backgroundColor = BCColor.BarTint.white
-    if let image = book?.image {
-      coverImageView.kf.setImage(with: URL(string: image))
-    }
-
-    headView.addSubview(coverImageView)
-    
-    coverImageView.snp.makeConstraints { make in
-      make.size.equalTo(CGSize(width: 115, height: 161))
-      make.left.equalToSuperview().inset(16)
-      make.centerY.equalToSuperview()
-    }
-    // Title
-    let titleLabel = UILabel()
-    titleLabel.text = book?.title
-    titleLabel.font = UIFont.systemFont(ofSize: 17.0)
-    titleLabel.textColor = BCColor.BarTint.white
-    titleLabel.numberOfLines = 0
-
-    headView.addSubview(titleLabel)
-    
-    titleLabel.snp.makeConstraints { make in
-      make.left.equalTo(coverImageView.snp.right).offset(14)
-      make.right.lessThanOrEqualToSuperview().inset(15)
-      make.top.equalToSuperview().inset(16)
-    }
-    
+  var detailTexts: [String] {
     var items = [String]()
     
-    if let authors = book?.authors, !authors.isEmpty {
-      var author = String()
+    if let authors = book.authors, !authors.isEmpty {
+      var author = ""
       authors.forEach { author += $0 + " " }
       items.append("Author: \(author)")
     }
     
-    if let translators = book?.translators, !translators.isEmpty {
-      var translator = String()
+    if let translators = book.translators, !translators.isEmpty {
+      var translator = ""
       translators.forEach { translator += $0 + " " }
       items.append("Translator: \(translator)")
     }
     
-    if book?.publisher != nil { items.append("Publisher: \(book!.publisher!)") }
+    if book.publisher != nil { items.append("Publisher: \(book!.publisher!)") }
     
     if book?.publishedDate != nil { items.append("Published Date: \(book!.publishedDate!)") }
     
@@ -197,140 +154,35 @@ extension BCInfoViewController {
     } else if let isbn = book?.isbn10 {
       items.append("ISBN: \(isbn)")
     }
-    // Info text
-    let stackView = UIStackView(
-      arrangedSubviews: items.map { item in
-        let label = UILabel()
-        label.text = item
-        label.font = UIFont.systemFont(ofSize: 11.0)
-        label.textColor = BCColor.BarTint.white
-        label.numberOfLines = 0
-        
-        return label
-      }
-    )
-    stackView.axis = .vertical
-    stackView.spacing = 4
     
-    headView.addSubview(stackView)
-    
-    stackView.snp.makeConstraints { make in
-      make.top.equalTo(titleLabel.snp.bottom).offset(4)
-      make.left.right.equalTo(titleLabel)
-    }
-    // Mark button
-    let markButton = UIButton(type: .custom)
-    markButton.titleLabel?.font = UIFont.systemFont(ofSize: 12.0)
-    markButton.backgroundColor = BCColor.BarTint.white
-    
-    markButton.setTitle("Mark", for: .normal)
-    markButton.setTitle("Unmark", for: .selected)
-    markButton.setTitleColor(UIColor(HEX: 0x00A25B), for: .normal)
-    markButton.setTitleColor(UIColor(HEX: 0xB8B8B8), for: .selected)
-    markButton.layer.cornerRadius = 2.0
-    
-    markButton.addTarget(self, action: #selector(mark(_:)), for: .touchUpInside)
-    
-    headView.addSubview(markButton)
-    
-    markButton.snp.makeConstraints { make in
-      make.left.equalTo(titleLabel)
-      make.size.equalTo(CGSize(width: 70, height: 27))
-      make.top.equalTo(stackView.snp.bottom).offset(6)
-      make.bottom.equalToSuperview().inset(4)
-    }
-    
-    markButton.isSelected = isMarked
-    
-    // ImageView layout
-    backgroundImageView.snp.makeConstraints { make in
-      make.top.centerX.width.equalToSuperview()
-      make.height.equalTo(headView).offset(topDistance)
-    }
-
-    // MARK: - The body of scroll view
-    let bodyView = UIView()
-    bodyView.backgroundColor = BCColor.BarTint.white
-    
-    scrollView.addSubview(bodyView)
-    
-    bodyView.snp.makeConstraints { make in
-      make.width.bottom.equalToSuperview()
-      make.top.equalTo(headView.snp.bottom)
-    }
-    
-    // summary
-    let summaryLabel = UILabel()
-    summaryLabel.text = "Summary"
-    summaryLabel.font = UIFont.systemFont(ofSize: 16.0)
-    summaryLabel.textColor = UIColor(HEX: 0x555555)
-    
-    bodyView.addSubview(summaryLabel)
-    
-    summaryLabel.snp.makeConstraints { make in
-      make.top.left.right.equalToSuperview().inset(15)
-    }
-    
-    // content detail
-    let detailLabel = UILabel()
-    detailLabel.numberOfLines = 0
-    detailLabel.font = UIFont.systemFont(ofSize: 15.0)
-    detailLabel.textColor = UIColor(HEX: 0x999999)
-    
-    detailLabel.text = book?.summary
-    
-    bodyView.addSubview(detailLabel)
-    
-    detailLabel.snp.makeConstraints { make in
-      make.left.right.bottom.equalToSuperview().inset(15)
-      make.top.equalTo(summaryLabel.snp.bottom).offset(6.5)
-    }
-    
-    view.layoutIfNeeded()
-    backgroundHeight = headView.frame.height + topDistance
+    return items
   }
-}
-
-// MARK: - Actions
-extension BCInfoViewController {
   
-  @objc
-  fileprivate func mark(_ sender: UIButton? = nil) {
-    if book == nil { return }
-    
-    if sender?.isSelected ?? false {
-      BCBookInfoService.unmark(by: book!.doubanID) {
-        BCResponse.handle($0, success: { id in
-          if id > 0 { sender?.isSelected.toggle() }
-        }) { V2RXError.printError($0) }
-      }
-    } else {
-      BCBookInfoService.mark(book!) {
-        BCResponse.handle($0, success: { id in
-          if id > 0 { sender?.isSelected.toggle() }
-        }) { V2RXError.printError($0) }
-      }
+  var summaryText: String {
+    book.summary ?? ""
+  }
+  
+  var isMarked: Bool { book.isMarked }
+  
+  func coverImageViewDidLoad(_ cover: UIImageView) {
+    if let imageURL = book.image {
+      cover.kf.setImage(with: URL(string: imageURL))
     }
   }
   
-  @objc
-  fileprivate func close(_ sender: UIBarButtonItem? = nil) {
-    dismiss(animated: true)
+  func didFinishingMark(by button: UIButton?) -> Bool {
+    book.isMarked.toggle()
+    
+    return book.isMarked
   }
-}
-
-// MARK: - ScrollView Delegate
-extension BCInfoViewController: UIScrollViewDelegate {
+  
   func scrollViewDidScroll(_ scrollView: UIScrollView) {
-    // Scroll down when content offset y is negative.
-    // Fix bug: in iOS 13, view frame is not show when frame has changed.
-    // but, it works for view.layer.frame!
-    backgroundImageView.layer.frame = CGRect(
+    infoView.backgroundImageView.layer.frame = CGRect(
       x: 0,
       y: 0,
       width: view.frame.width,
       height: scrollView.contentOffset.y < 0 ?
-        backgroundHeight - scrollView.contentOffset.y : backgroundHeight
+        infoView.backgroundHeight - scrollView.contentOffset.y : infoView.backgroundHeight
     )
   }
 }
